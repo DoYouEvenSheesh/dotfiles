ba261ee0d2aa53c2846973434e0a9d069f34928f
Dheeraj Reddy <92352131+DoYouEvenSheesh@users.noreply.github.com>
2025-01-26 11:44:37 +0530

    edit config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -U fish_greeting
-    fzf --fish | source
-    zoxide init fish | source
-    starship init fish | source
-    export PATH="$PATH:/opt/nvim-linux64/bin"
 end
 
 fish_add_path /home/poseidon/.spicetify
+alias ls "ls --color"
 alias wifilist "nmcli device wifi list"
 alias Zagreus "nmcli device wifi list && nmcli device wifi connect Zagreus"
 alias BITS-STUDENT "nmcli device wifi connect BITS-STUDENT"
 alias air "~/go/bin/air"
-alias vim "nvim"
+alias vim "nvim"    
+set -U fish_greeting
+fzf --fish | source
+starship init fish | source
+export PATH="$PATH:/opt/nvim-linux64/bin"
+zoxide init --cmd cd fish | source
