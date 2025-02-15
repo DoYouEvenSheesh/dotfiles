785db2e6b930b7c8b8db85b16e1e08b7f725c121
Dheeraj Reddy <92352131+DoYouEvenSheesh@users.noreply.github.com>
2025-02-15 09:05:46 +0530

    rm kbdlight & edit telscope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,8 @@ return {
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin.').live_grep)
+      vim.keymap.set("n", "<space>ff", require('telescope.builtin').find_files)
       vim.keymap.set("n", "<space>en", function()
 	require('telescope.builtin').find_files {
 	  cwd = vim.fn.stdpath("config")
