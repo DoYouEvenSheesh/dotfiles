83a0110610d46771e1e0ead999595d4c249232bb
Dheeraj Reddy <dheerajre@proton.me>
2025-05-23 21:28:24 +0530

    add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -U fish_greeting
+alias ls "ls --color"
+alias vim "nvim"
+fzf --fish | source
+starship init fish | source
+zoxide init --cmd cd fish | source
+alias ros "distrobox enter ros"
